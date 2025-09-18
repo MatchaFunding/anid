9f4df00dce94bfec0ca7b6386dc49cc0de5a9c44
ElMaikina <mikesoto890@gmail.com>
2025-09-18 11:18:56 -0300

    ultimos cambios

--- concursos/paginas/detalles/extraer_detalle_completo.sh
+++ concursos/paginas/detalles/extraer_detalle_completo.sh
@@ -1,5 +1,6 @@
 # Ejecuta todos los scripts generados
+FONDO=0
 set -e  # Termina en caso de error
 while read p; do
-	echo "Abriendo pagina del detalle: $p" &
+	wget $p --user-agent="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36" &
 done < enlaces_relevantes.txt
